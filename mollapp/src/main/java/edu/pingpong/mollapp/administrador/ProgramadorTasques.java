f7bdd182517daeeb502f1e0d549c99a4eb19f67a
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-04-27 20:07:32 +0200

    [ADD]: lódigca programadortasques

---
+++ mollapp/src/main/java/edu/pingpong/mollapp/administrador/ProgramadorTasques.java
@@ -0,0 +1,26 @@
+package main.java.edu.pingpong.mollapp.administrador;
+
+import main.java.edu.pingpong.mollapp.filtros.Filtre;
+import main.java.edu.pingpong.mollapp.tarjetas.Target;
+
+public class ProgramadorTasques {
+
+    public Tasques tasques = null;
+
+    public ProgramadorTasques(Target vehicle) {
+        tasques = new Tasques();
+        tasques.setTarget(vehicle);
+    };
+
+    public Tasques getTasques() {
+        return this.tasques;
+    };
+
+    public void setTasca(Filtre filtre) {
+        getTasques().afegirTasca(filtre);
+    };
+
+    public void executarTasques(String missatge) {
+        tasques.execucio(missatge);
+    };
+}
