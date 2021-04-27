edec348bd2f606775da806accdd474813e73b7e1
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-04-28 00:47:20 +0200

    [ADD]:  Casos test ProgramadorTasques

---
+++ mollapp/src/test/java/edu/pingpong/mollapp/administrador/ProgramadorTasquesTest.java
@@ -0,0 +1,17 @@
+package edu.pingpong.mollapp;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class ProgramadorTasquesTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
