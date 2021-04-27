283ecd72cbcdd9e1404578bf3e105049f4b253ac
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-04-27 17:53:18 +0200

    [IMP]: Rename

---
+++ mollapp/src/main/java/edu/pingpong/mollapp/Mollapp.java
@@ -0,0 +1,13 @@
+package edu.pingpong.mollapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class Mollapp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
